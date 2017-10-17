308e94e605203e216f845c1c5740bfba9d02d0fb
Yethiel <marvin.thiel@stud.uni-goettingen.de>
2017-10-17 20:13:10 +0200

    remove tests folder

--- tests/read_ncp.py
+++
@@ -1,7 +0,0 @@
-import rvstruct
-import json
-
-# with open("closed_n.ncp", "rb") as f:
-with open("muse1.ncp", "rb") as f:
-    ncp = rvstruct.NCP(f)
-    print(json.dumps(ncp.as_dict(), indent=2))
