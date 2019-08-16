a33a5f8fab5c271987d2b45b68e9b5b6a843d745
huki <gk7huki@gmail.com>
2019-08-16 10:48:45 +0530

    update version

--- io_revolt/__init__.py
+++ io_revolt/__init__.py
@@ -113,7 +113,7 @@ dprint("---\n\n\n\n")
 bl_info = {
 "name": "Re-Volt",
 "author": "Marvin Thiel",
-"version": (18, 8, 1),
+"version": (19, 8, 16),
 "blender": (2, 79, 0),
 "location": "File > Import-Export",
 "description": "Import and export Re-Volt file formats.",
